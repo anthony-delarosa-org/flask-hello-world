ca41f5a97a09344d15551445cdb4effd7449dc2d
AD <adelarosa@email.com>
2023-02-05 20:23:16 +0000

    changing app image

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/hello/')
 def hello_world():
-    return 'Hello Engineers!\n'
+    return 'Hello BusyQA Engineers!\n'
 
 @app.route('/hello/<username>') # dynamic route
 def hello_user(username):
